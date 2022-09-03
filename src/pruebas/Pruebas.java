471b1c766b119ff7d6cfa26d04c83fac17e2969a
jhordanasd4 <jhordancitopro24@gmail.com>
2022-09-02 20:46:42 -0500

    holasss

---
+++ src/pruebas/Pruebas.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pruebas;
+
+/**
+ *
+ * @author pc de jhordan
+ */
+public class Pruebas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hola pápu");
+        // TODO code application logic here
+    }
+    
+}
